7114e170d8541f11d7b4caa22eaef234ed906ae1
muffinman007 <cs70falls218@hotmail.com>
2015-03-29 13:15:26 -0700

    Add a TextBox Field for user to enter their user name. Add drop down box for user to enter the server ip address. (The program is still in testing form)

--- P2Crypt/Network/MainWindow.xaml.cs
+++ P2Crypt/Network/MainWindow.xaml.cs
@@ -20,8 +20,31 @@ namespace Network {
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow:Window {
+		
+		//P2Crypt.CryptoUtils cryptoUtils;
+		P2Crypt.PublicProfile publicProfile;
+		//P2Crypt.StandardAlgorithms standardAlgorithms;
+		P2Crypt.UserAccount userAccount;
+
 		public MainWindow() {
 			InitializeComponent();
+
+			for(int i = 0; i <= 255; ++i){							// populate each of the ip address section's drop down box
+				ipFirstByte.Items.Add(i);
+				ipSecondByte.Items.Add(i);
+				ipThirdByte.Items.Add(i);
+				ipFourthByte.Items.Add(i);
+			}
+
+			// set the each of the ip address section to 0
+ 			ipFirstByte.SelectedIndex = 0;
+			ipSecondByte.SelectedIndex = 0;
+			ipThirdByte.SelectedIndex = 0;
+			ipFourthByte.SelectedIndex = 0;
+
+				
 		}
+
+
 	}
 }
