c1a568f9e6ad99ff55f867451a79d250db5729c6
muffinman007 <cs70falls218@hotmail.com>
2015-03-29 17:37:53 -0700

    Add a new Server class that will handle incoming and outgoing connection. Also update the GUI to included a ip port text field.

--- P2Crypt/Network/MainWindow.xaml.cs
+++ P2Crypt/Network/MainWindow.xaml.cs
@@ -81,5 +81,7 @@ namespace Network {
 
 
 
+
+
 	}
 }
---
+++ P2Crypt/Network/Server.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Network{
+	/// <summary>
+	/// Control and handles incoming/outgoing packet. 
+	/// </summary>
+	public sealed class Server {
+
+		#region Fields
+		// singleton 
+		static Server instance = null;
+		static readonly object myLock = new object();
+
+		Socket socket;
+		#endregion
+
+
+		Server(){}
+
+		#region Properties
+		public static Server Instance{
+			get{
+				lock(myLock){
+					if(instance == null)
+						instance = new Server();
+					return instance;
+				}
+			}
+		}
+		#endregion
+
+
+		/// <summary>
+		/// Keep running until the parent thread send's a cancellation notice
+		/// </summary>
+		/// <param name="cancelToken"></param>
+		public void Run(CancellationToken cancelToken){
+			socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			socket.Bind(
+			
+			while(!cancelToken.IsCancellationRequested){
+				
+
+			}
+		}
+
+	}
+}
