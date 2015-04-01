fd5e73360316383439061e4604f6930bb3658bfc
muffinman007 <cs70falls218@hotmail.com>
2015-04-01 14:10:01 -0700

    Update NetworkServer structures. Add fields and a Package property.

--- src/P2CNetwork/P2CNetwork/NetworkServer.cs
+++ src/P2CNetwork/P2CNetwork/NetworkServer.cs
@@ -1,9 +1,70 @@
-﻿using System;
+﻿#region Header
+
+/**
+ * 
+ * Controls outgoing/incoming data for the program.
+ * 
+ **/
+
+#endregion Header
+
+using System;
 
 
 namespace P2CNetwork 
 {
-	class NetworkServer 
+	public class NetworkServer 
 	{
+
+		#region Fields
+		//switch
+		bool hasPackage;							// when NetworkServer received data and finish de-serializing it this turn to true
+		bool hasServerStart;
+
+		int port;
+		Package package;
+
+		#endregion Fields
+
+
+		#region Constructors
+
+		public NetworkServer(int port = 12345)
+		{
+			this.port = port;
+			hasPackage = false;
+			hasServerStart = false;
+		}
+
+		#endregion Constructors
+
+
+		#region Properties
+		public Package Package
+		{
+			get
+			{
+				if(hasPackage)
+					return package;
+				else{
+					return null;						// should we return an empty package?
+				}
+			}
+
+			private set
+			{
+				package = value;
+			}
+		}
+
+
+		#endregion Properties
+
+
+		#region Methods
+
+
+		#endregion Methods
+
 	}
 }
