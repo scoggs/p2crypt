d821da8cccbf4cc420614033c29ff991443b9dd2
muffinman007 <cs70falls218@hotmail.com>
2015-04-01 13:52:27 -0700

    Add IPackage to P2CCommon Project

---
+++ src/P2CCommon/P2CCommon/IPackage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2CCommon 
+{
+	public interface IPackage 
+	{
+		IPublicProfile PublicProfile{ get; private set; }
+ 
+		byte[] Data{ get; private set; }
+
+	}
+}
